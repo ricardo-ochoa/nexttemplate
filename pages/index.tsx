59f3f795a0f001c7420321ecd05cd2dc8df27e7e
Ochoagram <ricardogochoa@me.com>
2023-04-07 16:39:02 -0500

    first

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const hello = 'HelloWORDL'
+
   return (
     <>
       <Head>
